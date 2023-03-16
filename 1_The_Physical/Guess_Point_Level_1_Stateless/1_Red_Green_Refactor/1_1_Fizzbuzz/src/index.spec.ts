09814ca567d34dbcb8e1f9f6a9c2c87de7a15c00
Justin Calleja <justin.calleja@playnorth.com>
2023-03-16 23:47:49 +0100

    GREEN initial impl

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,15 @@
+import fizzbuzz from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  test.each([
+    ["1", 1],
+    ["2", 2],
+    ["Fizz", 3],
+    ["Buzz", 5],
+    ["FizzBuzz", 15],
+    ["FizzBuzz", 30],
+    ["31", 31],
+  ])("returns %p for input %p", (expected, input) => {
+    expect(fizzbuzz(input)).toEqual(expected);
+  });
 });
