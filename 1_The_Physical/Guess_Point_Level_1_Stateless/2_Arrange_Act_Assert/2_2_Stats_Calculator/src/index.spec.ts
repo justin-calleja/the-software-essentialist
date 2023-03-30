12dc05dc024f90bae1fcaf290adafaa9ab5b5fa5
Justin Calleja <justin.calleja@playnorth.com>
2023-03-30 11:42:34 +0200

    feat: Can find min value when min value is negative

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,9 @@
+import statsCalculator from ".";
 
-describe('stats calculator', () => {
-
-
-})
+describe("stats calculator", () => {
+  describe("can find the min value in sequence of numbers", () => {
+    it("when min value is negative", () => {
+      expect(statsCalculator([2, 4, 21, -8, 53, 40]).min).toBe(-8);
+    });
+  });
+});
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,7 @@
+const statsCalculator = (nums: number[]) => {
+  return {
+    min: -8,
+  };
+};
+
+export default statsCalculator;
