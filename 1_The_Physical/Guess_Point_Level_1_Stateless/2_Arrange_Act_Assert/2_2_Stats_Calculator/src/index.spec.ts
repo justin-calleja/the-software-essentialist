560337b26e67248b1540a96b6f64dccc637aa40d
Justin Calleja <justin.calleja@playnorth.com>
2023-03-30 12:08:40 +0200

    feat: Can find min value when min value is 0

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -5,5 +5,8 @@ describe("stats calculator", () => {
     it("when min value is negative", () => {
       expect(statsCalculator([2, 4, 21, -8, 53, 40]).min).toBe(-8);
     });
+    it("when min value is 0", () => {
+      expect(statsCalculator([2, 4, 21, 0, 53, 40]).min).toBe(0);
+    });
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -1,6 +1,20 @@
-const statsCalculator = (nums: number[]) => {
+export type Stats = {
+  min?: number;
+};
+
+const calcMin = (num1: number, num2: number) => {
+  return num1 < num2 ? num1 : num2;
+};
+
+const statsCalculator = (nums: number[]): Stats => {
+  let min: Stats["min"];
+
+  for (let i = 0; i < nums.length; i++) {
+    min = calcMin(min ?? Infinity, nums[i]);
+  }
+
   return {
-    min: -8,
+    min,
   };
 };
 
