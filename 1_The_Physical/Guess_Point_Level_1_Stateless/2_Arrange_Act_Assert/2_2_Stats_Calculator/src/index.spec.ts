229fae7169de027155191729f116314fb8500322
Justin Calleja <justin.calleja@playnorth.com>
2023-03-30 13:24:47 +0200

    chore: add random gen test for min

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,3 +1,4 @@
+import fc from "fast-check";
 import statsCalculator from ".";
 
 describe("stats calculator", () => {
@@ -5,8 +6,27 @@ describe("stats calculator", () => {
     it("when min value is negative", () => {
       expect(statsCalculator([2, 4, 21, -8, 53, 40]).min).toBe(-8);
     });
+
     it("when min value is 0", () => {
       expect(statsCalculator([2, 4, 21, 0, 53, 40]).min).toBe(0);
+    });
+
+    it("when seq of numbers is randomnly generated ", () => {
+      const expected = -1;
+
+      fc.assert(
+        fc.property(
+          // generate an array with length 1 to 11 with numbers from 0 to 10:
+          fc.array(fc.integer({ min: 0, max: 10 }), {
+            minLength: 1,
+            maxLength: 11,
+          }),
+
+          (randNumbers) => {
+            return statsCalculator([...randNumbers, expected]).min === expected;
+          }
+        )
+      );
     });
   });
 });
